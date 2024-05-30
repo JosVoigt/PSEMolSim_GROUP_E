1f866714a037598a0f439b3e373f934b6ddf5a68
Joshua Voigt <joshua.voigt@tum.de>
2024-05-30 22:49:50 +0200

    fixed the segfault issue

--- test/test_suite_all.cpp
+++ test/test_suite_all.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
-
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 int main (int argc, char* argv[]) {
+  auto console_logger = spdlog::stderr_color_mt("console");
+  try {
+    std::stringstream ss;
+    std::string time_format = "%Y-%m-%d_%H-%M-%S";
+
+    auto start = std::chrono::system_clock::now();
+    time_t start_time = std::chrono::system_clock::to_time_t(start);
+
+    ss << "logs/MolSim_"
+       << std::put_time(std::localtime(&start_time), time_format.c_str())
+       << ".log";
+
+    auto file_logger = spdlog::basic_logger_mt("file", ss.str());
+  }
+  catch (const spdlog::spdlog_ex &ex) {
+    spdlog::get("console")->critical("File logger could not be initalizied: {}",
+                                     ex.what());
+    exit(1);
+  }
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
