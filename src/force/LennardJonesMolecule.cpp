3e5a51149cfb7e560161929c78017031d5ea7176
Joshua Voigt <joshua.voigt@tum.de>
2024-05-31 01:52:05 +0200

    reverted one test case§

--- src/force/LennardJonesMolecule.cpp
+++ src/force/LennardJonesMolecule.cpp
@@ -1,32 +1,36 @@
 #include "LennardJonesMolecule.h"
 
-#include <cmath>
 #include <spdlog/spdlog.h>
+
+#include <cmath>
+
 #include "utils/ArrayUtils.h"
 
 LennardJonesMolecule::LennardJonesMolecule(double epsilon_, double sigma_)
     : epsilon(epsilon_), sigma(sigma_) {
-    epsilon_24 = epsilon * (-24);
+  epsilon_24 = epsilon * (-24);
 }
 
 std::array<double, 3> LennardJonesMolecule::calculateForce(Particle& p1,
                                                            Particle& p2) const {
-    double distance = ArrayUtils::L2Norm(p1.getX() - p2.getX());
+  double distance = ArrayUtils::L2Norm(p1.getX() - p2.getX());
 
-    double summand_6 = std::pow((sigma / distance), 6);
-    double summand_12 = std::pow(summand_6, 2);
+  double summand_6 = std::pow((sigma / distance), 6);
+  double summand_12 = std::pow(summand_6, 2);
 
-    std::array<double, 3> force = (epsilon_24 / std::pow(distance, 2)) *
-                                  (summand_6 - (2 * summand_12)) *
-                                  (p1.getX() - p2.getX());
+  std::array<double, 3> force = (epsilon_24 / std::pow(distance, 2)) *
+                                (summand_6 - (2 * summand_12)) *
+                                (p1.getX() - p2.getX());
 
 #ifndef NO_LOG
-    spdlog::get("file")->trace ("Calculating force LenJonesMol: s_6: {}, s_12: {}, dist: {}, force: {}", summand_6, summand_12, distance, ArrayUtils::to_string(force));
+  spdlog::get("file")->trace(
+      "Calculating force LenJonesMol: s_6: {}, s_12: {}, dist: {}, force: {}",
+      summand_6, summand_12, distance, ArrayUtils::to_string(force));
 #endif
 
-    return force;
+  return force;
 }
 
 std::string LennardJonesMolecule::typeString() {
-    return "Lennard-Jones-Potential for molecules";
+  return "Lennard-Jones-Potential for molecules";
 }
