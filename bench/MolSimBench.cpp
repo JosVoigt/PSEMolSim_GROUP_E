e0206e659ab5cf1d1c1ec93629a39657e6579e6a
Joshua Voigt <joshua.voigt@tum.de>
2024-06-17 15:32:15 +0200

    added bench to build and checked for logging

--- bench/MolSimBench.cpp
+++ bench/MolSimBench.cpp
@@ -1,20 +1,25 @@
 #include <benchmark/benchmark.h>
+#include <iostream>
 
 #include "container/Particle.h"
 #include "force/LennardJonesForce.h"
 
+#ifndef NO_LOG
+int main () {
+	std::cout << "Please disable logging to run the benchmarks. To do so call cmake with -DLOGGING=OFF" << std::endl;
+	return 0;
+}
+#else
 static void testBench(benchmark::State& state) {
   LennardJonesForce ljf = LennardJonesForce(1, 1);
 
   std::array<double, 3> zero = {0, 0, 0};
   Particle p = Particle(zero, zero, 0);
   Particle inc = Particle(zero, zero, 0);
-  std::array<double, 3> one = {1, 0, 0};
-
-  for (int i = 0; i < 10000; i++) {
-    inc.addX(one);
+  for (auto _ : state) {
     ljf.calculateForce(p, inc);
   }
 }
 
 BENCHMARK(testBench);
+#endif
