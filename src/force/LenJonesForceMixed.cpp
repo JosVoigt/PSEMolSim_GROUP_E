724990269457ad9906972761dc7e421b70b4c39b
Joshua Voigt <joshua.voigt@tum.de>
2024-06-20 16:05:15 +0200

    added tests and fixed mixing

--- src/force/LenJonesForceMixed.cpp
+++ src/force/LenJonesForceMixed.cpp
@@ -2,67 +2,37 @@
 
 #include <spdlog/spdlog.h>
 
+#include <algorithm>
 #include <array>
 #include <cmath>
+#include <set>
+#include <tuple>
 #include <utility>
 #include <vector>
 
 #include "container/Particle.h"
 #include "utils/ArrayUtils.h"
 
-LennardJonesForceMixed::LennardJonesForceMixed(std::vector<double> epsilon,
-                                               std::vector<double> sigma,
-                                               std::vector<int> type) {
-  if (epsilon.size() != sigma.size() || epsilon.size() != type.size() || sigma.size() != type.size()){
+LennardJonesForceMixed::LennardJonesForceMixed(std::vector<double> epsilon_,
+                                               std::vector<double> sigma_,
+                                               std::vector<int> type_) {
+  if (epsilon.size() != sigma.size() && sigma.size() != type.size()) {
     spdlog::get("console")->critical(
         "Provided LenJones constant list length does not match up. \n"
-        "Lengths were: epsilon {} sigma {} type {}",
+        "Lengths were: epsilson {} sigma {} type {}",
         epsilon.size(), sigma.size(), type.size());
     exit(1);
   }
 
-  while (!type.empty()) {
-    // find the lowest element
-    int min_idx = 0;
-    int min = type[0];
-    for (int i = 0; i < type.size(); i++) {
-      if (min > type[i]) {
-        min_idx = i;
-        min = type[i];
-      }
-    }
-
-    // insert all min_idx combinations
-    for (int i = 0; i < type.size(); i++) {
-      int hashpos = z_curve_interleave(i, min);
-      force_constants.insert_or_assign(
-          hashpos, calculateMixingRule(sigma[i], epsilon[i], sigma[min_idx],
-                                       epsilon[min_idx]));
-    }
-
-    // remove from the three lists
-    epsilon.erase(epsilon.begin() + min_idx);
-    sigma.erase(sigma.begin() + min_idx);
-    type.erase(type.begin() + min_idx);
-  }
-}
-
-int inline LennardJonesForceMixed::z_curve_interleave(int a, int b) {
-  // interleave with 0
-  a *= a;
-  b *= b;
-
-  // bitshift one by 1
-  b <<= 1;
-
-  // interleave
-  return (a | b);
+  epsilon = epsilon_;
+  sigma = sigma_;
+  type = type_;
 }
 
 std::pair<double, double> LennardJonesForceMixed::calculateMixingRule(
     double sig_a, double eps_a, double sig_b, double eps_b) {
   //already precalc the 24 for later
-  double eps_mixed = 24 * std::sqrt(eps_a * eps_b);
+  double eps_mixed = std::sqrt(eps_a * eps_b);
   double sig_mixed = (sig_a + sig_b) / 2;
 
   return {sig_mixed, eps_mixed};
@@ -74,34 +44,32 @@ std::array<double, 3> LennardJonesForceMixed::calculateForce(
   int type1 = p1.getType();
   int type2 = p2.getType();
 
-  int position;
-  // interleave
-  if (type1 > type2) {
-    position = z_curve_interleave(type1, type2);
-  } else {
-    position = z_curve_interleave(type2, type1);
+  int pos1, pos2 = 0;
+
+  for (int i = 0; i < type.size(); i++) {
+    if (type[i] == type1) {
+      pos1 = i;
+    }
+    if (type[i] == type2) {
+      pos2 = i;
+    }
   }
 
-  // fetch data from hashmap
-  std::pair<double, double> sig_eps = force_constants.find(position)->second;
+  std::pair<double, double> sig_eps = calculateMixingRule(
+      sigma[pos1], epsilon[pos1], sigma[pos2], epsilon[pos2]);
+
   double sig = sig_eps.first;
   double eps = sig_eps.second;
 
-  // calculate distance
-  std::array<double, 3> x1 = p1.getX();
-  std::array<double, 3> x2 = p2.getX();
-  std::array<double, 3> vector1_2 = x1 - x2;
-  double distance = ArrayUtils::L2Norm(vector1_2);
+  std::array<double, 3> vector = (p1.getX() - p2.getX());
+  double distance = ArrayUtils::L2Norm(vector);
+  double summand_6 = std::pow((sig / distance), 6);
+  double summand_12 = std::pow((sig / distance), 12);
 
-  // lenjones
-  double sig_dist_6 = std::pow(sig / distance, 6);
-  std::array<double, 3> force = -eps / std::pow(distance, 2) *
-                                (sig_dist_6 - (2 * std::pow(sig_dist_6, 2))) *
-                                vector1_2;
+  std::array<double, 3> force = -24 * (eps / std::pow(distance, 2)) *
+                                (summand_6 - (2 * summand_12)) * vector;
 
   return force;
 }
 
-std::string LennardJonesForceMixed::typeString() {
-    return {};
-}
+std::string LennardJonesForceMixed::typeString() { return {}; }
--- src/force/LenJonesForceMixed.h
+++ src/force/LenJonesForceMixed.h
@@ -1,5 +1,4 @@
-
-#include <map>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -8,15 +7,42 @@
 
 class LennardJonesForceMixed : public PairwiseForce {
  private:
-  std::map<int, std::pair<double, double>> force_constants;
+	 std::vector<double> epsilon;
+	 std::vector<double> sigma;
+	 std::vector<int> type;
 
   static int inline z_curve_interleave(int a, int b) ;
   static std::pair<double, double> calculateMixingRule(double sig_a, double eps_a,
                                                 double sig_b, double eps_b);
 
  public:
+  /**
+   * \brief
+   * Initializer for the force calculation.
+   * The constructor will precalc the values for all possible combinations.
+   *
+   * It will use the fact that Lorenz-Berthelot mixing rule yields the same result for particle combinations and their mirror composition
+   *
+   * \param epsilon
+   * The epsilon values for the different particle types
+   * \param sigma
+   * The sigma values for the different particle types
+   * \param type
+   * The type of the particles. The length of all lists has to match
+   */
   LennardJonesForceMixed(std::vector<double> epsilon, std::vector<double> sigma,
                          std::vector<int> type);
+  /**
+   * \brief
+   *  Calculates the force between two particles, taking their types into consideration
+   *  Force will point from p1 to p2
+   * \param p1
+   * The first particle
+   * \param p2
+   * The second particle
+   * \return
+   * The force attracting/repelling p1 to/from p2
+   */
   std::array<double, 3> calculateForce(Particle& p1,
                                        Particle& p2) const override;
 
