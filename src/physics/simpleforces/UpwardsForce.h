b19292fb5c477c974bdf79c1863118f64c8bf549
ge96jon <ge96jon@mytum.de>
2024-07-03 12:46:55 +0200

    Added upwards force

---
+++ src/physics/simpleforces/UpwardsForce.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class UpwardsForce : public SimpleForceSource {
+
+    /**
+     * @brief Calculates the small upwards force for the given particle
+     * @param p Particle
+     * @return std::array<double, 3UL> Force vector
+     */
+    std::array<double, 3UL> calculateForce(Particle& p) const override;
+};
