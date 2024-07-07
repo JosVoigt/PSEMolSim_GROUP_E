66eaf2910bf45070cb6e6b0e3b704c1b3bd54aac
Luca <ge96hik@mytum.de>
2024-07-07 20:32:54 +0200

    added MembraneSpawner to XMLFileReader

--- src/io/input/xml/XMLFileReader.cpp
+++ src/io/input/xml/XMLFileReader.cpp
@@ -140,6 +140,12 @@ std::tuple<std::vector<Particle>, SimulationParams> prepareParticles(std::filesy
         Logger::logger->info("Spawned {} particles from cuboid spawner", num_spawned);
     }
 
+    for (auto membrane_spawner : particle_sources.membrane_spawner()) {
+        auto spawner = XSDToInternalTypeAdapter::convertToMembraneSpawner(membrane_spawner, settings.third_dimension());
+        int num_spawned = spawner.spawnParticles(particles);
+        Logger::logger->info("Spawned {} particles from membrane spawner", num_spawned);
+    }
+
     for (auto sphere_spawner : particle_sources.sphere_spawner()) {
         auto spawner = XSDToInternalTypeAdapter::convertToSphereSpawner(sphere_spawner, settings.third_dimension());
         int num_spawned = spawner.spawnParticles(particles);
