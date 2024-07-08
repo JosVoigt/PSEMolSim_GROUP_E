f7790913dab1df97def33e577c6e9f208615064f
Luca <ge96hik@mytum.de>
2024-07-08 21:38:56 +0200

    changed UpwardsForce

--- src/physics/simpleforces/UpwardsForce.cpp
+++ src/physics/simpleforces/UpwardsForce.cpp
@@ -1,5 +1,17 @@
 #include "UpwardsForce.h"
 
-std::array<double, 3UL> UpwardsForce::calculateForce(Particle& p) const {
-    return {0.0, 0.0, 0.8};
+UpwardsForce::UpwardsForce(std::array<double, 3> force, std::vector<int>& target_particles, double end_time)
+: force(force), target_particles(target_particles), end_time(end_time) {}
+
+void UpwardsForce::calculateForce(std::vector<Particle>& particle_vector, double time) const {
+    if(time < end_time) {
+        for(auto& t : target_particles) {
+            Particle p = particle_vector[t];
+            std::array<double, 3> newForce{};
+            for(int i = 0; i < 3; i++) {
+                newForce[i] = p.getF()[i] + force[i];
+            }
+            p.setF(newForce);
+        }
+    }
 }
--- src/physics/simpleforces/UpwardsForce.h
+++ src/physics/simpleforces/UpwardsForce.h
@@ -2,12 +2,18 @@
 
 #include "SimpleForceSource.h"
 
-class UpwardsForce : public SimpleForceSource {
+class UpwardsForce {
+private:
+    std::array<double, 3> force;
+    std::vector<int> target_particles;
+    double end_time;
 
+public:
+    UpwardsForce(std::array<double, 3> force, std::vector<int>& target_particles, double end_time);
     /**
      * @brief Calculates the small upwards force for the given particle
-     * @param p Particle
-     * @return std::array<double, 3UL> Force vector
+     * @param p Particle vector
+     * @param time Current time
      */
-    std::array<double, 3UL> calculateForce(Particle& p) const override;
+    void calculateForce(std::vector<Particle>& p, double time) const;
 };
